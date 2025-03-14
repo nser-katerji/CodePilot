31144086dfacde1451f3be0e4bd873cacd927679
Nser Katerji <nser.katerji@ingka.ikea.com>
2025-03-14 13:33:01 +0100

    PR2

---
+++ Test.py
@@ -0,0 +1,9 @@
+def find_multiples_in_range(number, start, end):
+    """Finds all multiples of 'number' in a given range [start, end]."""
+    return [i for i in range(start, end + 1) if i % number == 0]
+
+# Example usage:
+num = int(input("Enter a number: "))
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
+print(f"Multiples of {num} between {start} and {end}: {find_multiples_in_range(num, start, end)}")
